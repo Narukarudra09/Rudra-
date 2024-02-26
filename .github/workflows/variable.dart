8d04ab469e0747442b8216a9b1da26d9bf12f717
Narukarudra09 <160457532+Narukarudra09@users.noreply.github.com>
2024-02-26 10:28:16 +0530

    Add files via upload

---
+++ .github/workflows/variable.dart
@@ -0,0 +1,20 @@
+void main()
+{
+  int a=5;
+
+  print(a);
+
+  double pi=3.14;
+
+  print(pi);
+
+  String name="rudra";
+
+  print(name);
+
+  print('data type is auto');
+
+  var num=20;
+
+  print(num);
+}
